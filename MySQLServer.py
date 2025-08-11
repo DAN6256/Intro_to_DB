cf1bb1ec5ee687fcd2d26d479c60bc1c04fcd841
Daniel Tunyinko <daniel.tunyinko@gmail.com>
2025-08-11 17:34:58 +0000

    Create database with python

---
+++ MySQLServer.py
@@ -0,0 +1,14 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",user="root", password=""
+)
+
+mycursor = mydb.cursor()
+mycursor.execute("""
+    CREATE DATABASE IF NOT EXISTS alx_book_store
+""")
+
+print("Database 'alx_book_stor' created successfully")
+mycursor.close()
+mydb.close()
